7ca674968d073076ee886512382c013f87b32876
Harsh641-1 <187022749+Harsh641-1@users.noreply.github.com>
2025-10-25 12:00:18 +0530

    Exercise 1.11 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,15 +12,18 @@ const Statistics = (props) => {
   }
 
   return (
-    <div>
-       <h1>statistics</h1>
+   <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
       <StatisticsLine text='good' value={props.good} />
       <StatisticsLine text='neutral' value={props.neutral} />
       <StatisticsLine text='bad' value={props.bad} />
       <StatisticsLine text='all' value={props.total} />
       <StatisticsLine text='average' value={props.average} />
       <StatisticsLine text='positive' value={props.positive} />
-      
+      </tbody>
+      </table>
   </div>
   )
 
@@ -28,7 +31,7 @@ const Statistics = (props) => {
 
 const StatisticsLine = (props) => {
   return (
-    <p>{props.text}: {props.value}</p>
+    <tr><td>{props.text}:</td><td>{props.value}</td></tr>
   )
 }
 
